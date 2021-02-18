f27532dc3164eddeacf0710b3d6ceac5ae6a2450
Ted95On <65019880+Ted95On@users.noreply.github.com>
2021-02-18 00:24:53 +0000

    Layout overhaul, fixes and optimisations
    Fixed the layout, should now display properly across all systems. Reworked how user settings is read and saved.

--- ACBSetupTool.py
+++ ACBSetupTool.py
@@ -4,32 +4,24 @@ import tkinter as tk
 from tkinter import ttk, messagebox, font
 from tkinter.filedialog import askdirectory
 import subprocess
+import csv
 
 import webbrowser
 import gettext
 _ = gettext.gettext
 
-version = 0.1
+version = 1.0 # Map functionality complete
 
-#global gamePath
+savefile = 'settings.ini'
+ACBSetupTool = "ACBSetupTool.py"
+userStruct = ('Language', 'GamePath')
+defaultValues = ('en,notdefined')
+
 default1 = "C:/Program Files (x86)/Steam/steamapps/common/Assassins Creed Brotherhood"              #default Steam directory
 default2 = "C:/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/games/Assassins Creed Brotherhood" #default UbisoftConnect/Uplay directory			
 
 defaultpathExist = path.exists(default1+"/ACBSP.exe")
 default2pathExist = path.exists(default2+"/ACBSP.exe")
-
-if defaultpathExist:
-	pathExist = True
-	gamePath = default1
-
-elif default2pathExist:
-	pathExist = True
-	gamePath = default2	
-
-else:
-	pathExist = False
-	gamePath = ""
-	print("Can't find directory")
 
 #links
 ANwebsite = 'https://assassins.network'
@@ -55,53 +47,65 @@ maps = {
 		"Rome": "DataPC_ACR_Rome_Multi.forge"
 		}
 
-filename = ('settings.ini')
-doesitexist = path.exists(filename)
-if doesitexist == True:
-	filestate = os.stat("settings.ini").st_size == 0
+def checkDefaultPath():
 
-def readSettings():
-	if doesitexist == False:
-		createfile = open(filename, "w+")
-		createfile.write("File ok\n")
-		createfile.write("en\n")
-		createfile.close()
-		return "en"
+	if defaultpathExist:
+		pathExist = True
+		gamePath = default1
 
-	elif doesitexist == True and filestate == True:
-		writetofile = open(filename, "w")
-		writetofile.write("File ok\n")
-		writetofile.write("en\n")
-		return "en"
+	elif default2pathExist:
+		pathExist = True
+		gamePath = default2	
 
 	else:
-		try:
-			opensettings = open(filename, "r")
-			count = 0
-			for i in opensettings:
-				if count == 1:
-					lang = i.rstrip("\n")
-				count+=1
-			opensettings.close()
-			return lang
-		except:
-			return "en"
+		pathExist = False
+		gamePath = ""
+		print("Can't find default directory")
+		
+	return pathExist, gamePath
 
+def readUserData():
+	if path.exists(savefile):
+		with open(savefile, 'r') as file:
 
-def replace_line(file_name, line_num, text):
-	linesGet = open(file_name, "r")
-	lines = linesGet.readlines()
-	lines[line_num] = text+"\n"
-	out = open(file_name, "w")
-	out.writelines(lines)
-	out.close()
-	linesGet.close()   
+			reader = csv.DictReader(file, fieldnames=userStruct)
+			userData = dict()
 
-getLang = readSettings()
+			for row in reader:
+				userData = row
+			print(savefile + " file found.")
+			return userData
+	else:
+		print(savefile + " file not found.")
+		return newUser()
 
-lang = gettext.translation('base', localedir='locales', languages=[getLang])
-lang.install()
-_ = lang.gettext
+def newUser():
+	with open(savefile, 'w') as file:
+
+		file.write(defaultValues)
+		print(savefile + " successfully created!")
+
+	return readUserData()
+
+def writeUserData(data):
+	with open(savefile, 'w') as file:
+
+		writer = csv.DictWriter(file, fieldnames=userStruct)
+		writer.writerow(data)
+		print("Data saved!")
+
+def getLocalization():
+
+	try:
+		lang = gettext.translation('base', localedir='locales', languages=[getLang])
+		lang.install()
+
+	except:
+			print("Could not retrieve user language. Defaulting to English. Check your " + savefile + " file.")
+			lang = gettext.translation('base', localedir='locales', languages=["en"])
+			lang.install()
+
+	return lang.gettext
 
 ##########################################################tool start
 
@@ -116,68 +120,84 @@ def openLangMenu():
 	langLabel = tk.Label(langMenu, bg="#89b0b3", pady=5, text=_("Choose a language\n")).pack()
 
 	for language, val in languages:
+
     		ttk.Radiobutton(langMenu, 
         	text=language,
         	style="lang.TRadiobutton", 
             variable=v,
             value=val).pack(side="top", fill=tk.X, padx=100)
+
 	langButton = tk.Button(langMenu, text="Apply changes", bd=2, relief="raised", command=lambda: applyLang()).pack(side=tk.BOTTOM, pady=5)
 
 	def applyLang():
+
 		langMenu.destroy()
 		newLang = v.get()
-		if newLang != getLang:
-			replace_line(filename, 1, newLang)
-			os.startfile("ACBSetupTool.py")
+
+		if newLang != userData["Language"]:
+
+			userData["Language"] = newLang
+			writeUserData(userData)
+			os.startfile(ACBSetupTool)
 			root.destroy()
 
 def choosePath(): ########################## Manually sets the directory
 
 	ACBpath = askdirectory(title="Select your Assassin's Creed Brotherhood installation folder")
+
 	if path.exists(str(ACBpath)+"/ACBSP.exe") == False:
+
 		messagebox.showerror("Path Error", "Wrong directory!\nCan't find \"ACBSP.exe\" in selected folder.")
 	else:
-		with open(filename, "a") as file:
-			file.write(str(ACBpath) + "\n")
-			dirPath.set(ACBpath)
-			acbFolder['state'] = tk.DISABLED
-			os.startfile("ACBSetupTool.py")
-			root.destroy()
 
-def checkPathandStatus(gamePath): ######### Automatically searches for directory path and map status
-	if pathExist == True:
+		userData["GamePath"] = ACBpath
+		writeUserData(userData)
+		os.startfile(ACBSetupTool)
+		root.destroy()
+
+def checkPathandStatus(): ######### Automatically searches for directory path and map status
+
+	global gamePath
+
+	if path.exists(userData["GamePath"]+"/ACBSP.exe"):
+
+		gamePath = userData["GamePath"]
 		dirPath.set(gamePath)
-		acbFolder["state"] = tk.DISABLED
-		print("Directory found!")
-	elif pathExist == False:
-		with open(filename, "r") as info:
-			fileInfo = info.readlines()
+		acbFolder.config(state="disabled", text="Directory Set!")
+		print("User set directory")
 
-			try:
-				newPath = fileInfo[2].strip("\n")
-				if path.exists(str(newPath)+"/ACBSP.exe") == True:
-					gamePath = newPath
-					dirPath.set(gamePath)
-					acbFolder["state"] = tk.DISABLED
-			except:
-				messagebox.showinfo("No Directory Located", "No directory has been detected.\n\nClick on \"Set Directory\" and" +
+	elif pathExist:
+
+		dirPath.set(gamePath)
+		acbFolder.config(state="disabled", text="Directory Set!")
+		print("Default directory found!")
+
+	else:
+
+		messagebox.showinfo("No Directory Located", "No directory has been detected.\n\nClick on \"Set Directory\" and" +
 					" manually select your folder containing \"ACBSP.exe\".")
-				gamePath = ""
-				print("Directory not yet set")
-				applyChanges["state"] = tk.DISABLED
+		gamePath = ""
+		print("Directory not yet set")
+		applyChanges["state"] = tk.DISABLED
+		startGame["state"] = tk.DISABLED
 
-############################################################################ Checks status of each map
+#################### Checks status of each map
+
 	global mapStatus
 	mapStatus = dict()
-	for place, local in maps.items():
-		value = path.exists(gamePath+"/multi/" + local)
+	for mapName, mapFile in maps.items():
+
+		value = path.exists(gamePath+"/multi/" + mapFile)
+
 		if value == True:
-			mapStatus[place] = True
+			mapStatus[mapName] = True
 		else:
-			mapStatus[place] = False
-############################################################################
+			mapStatus[mapName] = False
+
+##############################################
 
 def switchState(mapName):   ################################################ Switches button state
+
 	global mapStatus
 	if mapName == "Alhambra":
 		if mapStatus[mapName] == True:
@@ -269,13 +289,19 @@ def switchState(mapName):   ################################################ Swi
 
 	changesFound = 0
 	for place, local in maps.items():
+
 		value = path.exists(gamePath+"/multi/" + local)
+
 		if value != mapStatus[place]:
 			changesFound+=1
+
 	if changesFound != 0:
+
 		unsavedChanges.set("You have unsaved changes.")
 		changeLabel.config(foreground="red")
+
 	else: unsavedChanges.set("")
+
 	if gamePath == "":
 		unsavedChanges.set("No directory has been set.")
 
@@ -283,54 +309,106 @@ def applyMapChanges():  ##### Applies the map changes by checking the difference
 						##### the map's existance and the local variable with the user set status
 	changesMade = 0
 	for place, local in maps.items():
+
 		value = path.exists(gamePath+"/multi/" + local)
+
 		if value != mapStatus[place] and value == True:
+
 			print("Map turned off: ", place)
 			os.rename(gamePath+"/multi/"+local,gamePath+"/multi/"+local+".OFF")
 			changesMade+=1
+
 		if value != mapStatus[place] and value == False:
+
 			print("Map turned on: ", place)
 			os.rename(gamePath+"/multi/"+local+".OFF",gamePath+"/multi/"+local)
 			changesMade+=1
 	
 	if changesMade != 0:
-		unsavedChanges.set("Your changes have been saved.")
+
+		unsavedChanges.set("Saved! Ready to play.")
 		changeLabel.config(foreground="green")
 
 def launchGame(gamePath):  ##### Launches the game
-	try:
-		user = ""
-		password = ""
-		startGame.config(state="disabled", text="Game running")
-		root.iconify()
-		subprocess.run([gamePath + "/ACBMP.exe", "/launchedfromotherexec", "/onlineUser:"+user, "/onlinePassword:"+password])
-		root.deiconify()
-		startGame.config(state="active", text="Launch Multiplayer")
-	except:
-		messagebox.showerror("Error Launching Game!", "Game not found.\n\nCheck your directory path.")
-		root.deiconify()
-		startGame.config(state="active", text="Launch Multiplayer")
-		
+	
+	info = tk.Toplevel(root)
+	info.geometry("250x100+300+300")
+	info.title("Login Info")
+	info.iconphoto(False, logo)
+	info.resizable(False, False)
+	info.grid_columnconfigure(0, weight=1)
+	info.grid_columnconfigure(3, weight=1)
+	info.grid_rowconfigure(0, weight=1)
+	info.grid_rowconfigure(4, weight=1)
+
+	labelUser = tk.Label(info, text="Username: ").grid(row=1, column=1)
+	userUser = tk.Entry(info)
+	userUser.grid(row=1, column=2)
+	labelPass = tk.Label(info, text="Password: ").grid(row=2, column=1, pady=(3,0))
+	userPass = tk.Entry(info, show="•")
+	userPass.grid(row=2, column=2, pady=(3,0))
+
+	playButton = tk.Button(info, text="Play", command=lambda: login()).grid(row=3, column=1, columnspan=2, pady=(10,0))
+
+	def login():
+	
+		try:
+			user = userUser.get()
+			password = userPass.get()
+			info.destroy()
+			startGame.config(state="disabled", text="Game running")
+			root.iconify()
+			subprocess.run([gamePath + "/ACBMP.exe", "/launchedfromotherexec", "/onlineUser:"+user, "/onlinePassword:"+password])
+			root.deiconify()
+			startGame.config(state="active", text="Launch Multiplayer")
+
+		except:
+			messagebox.showerror("Error Launching Game!", "Game not found.\n\nCheck your directory path.")
+			root.deiconify()
+			startGame.config(state="active", text="Launch Multiplayer")
+
+
+pathExist, gamePath = checkDefaultPath() ### Checks if the default game path exists and returns it
+print(pathExist, gamePath)
+
+userData = readUserData()				### Returns user info
+getLang = userData["Language"]			### Reads user language
+
+try:
+	_ = getLocalization()   			### Retrieves localization files
+except:
+	print("Could not find localization files. Check your locales folders.")
 
 root = tk.Tk()
 root.title(_("Assassin's Creed: Brotherhood Setup Manager Tool"))
-root.minsize(width=800, height=300)
-root.maxsize(width=800, height=300)
-root.resizable(False, False)
-root.geometry("800x300+250+200")
+root.minsize(height=0, width=150)
+root.resizable(True, False)
 
-logo = tk.PhotoImage(file = "images/icon.png")
-biglogo = tk.PhotoImage(file = "images/sidepanel.png")
-onIMG = tk.PhotoImage(file = "images/OnSwitch.png")
-offIMG = tk.PhotoImage(file = "images/OffSwitch.png")
-helle = tk.PhotoImage(file = "images/hellequin.png")
-fazz = tk.PhotoImage(file = "images/blacksmith.png")
-verify = tk.PhotoImage(file = "images/verify.png")
-jigaudi = tk.PhotoImage(file = "images/jigaudi.png")
-selectMaps = tk.PhotoImage(file = "images/selectmaps.png")
-root.iconphoto(False, logo)
+try:
+	logo = tk.PhotoImage(file = "images/icon.png")
+	sidepanel = tk.PhotoImage(file = "images/sidepanel.png")
+	onIMG = tk.PhotoImage(file = "images/OnSwitch.png")
+	offIMG = tk.PhotoImage(file = "images/OffSwitch.png")
+	engihelle = tk.PhotoImage(file = "images/engihelle.png")
+	fazz = tk.PhotoImage(file = "images/blacksmith.png")
+	verify = tk.PhotoImage(file = "images/verify.png")
+	jigaudi = tk.PhotoImage(file = "images/jigaudi.png")
+	courty = tk.PhotoImage(file = "images/courty.png")
+	fouadIMG = tk.PhotoImage(file = "images/fouad.png")
+	selectMaps = tk.PhotoImage(file = "images/selectmaps.png")
+	tabHome = tk.PhotoImage(file = "images/tabHome.png")
+	tabMap = tk.PhotoImage(file = "images/tabMap.png")
+	tabBoot = tk.PhotoImage(file = "images/tabBoot.png")
+	tabSettings = tk.PhotoImage(file = "images/tabSettings.png")
+	tabAcc = tk.PhotoImage(file = "images/tabAcc.png")
+	launchMP = tk.PhotoImage(file = "images/launchMP.png")
+	applyChangesIMG = tk.PhotoImage(file = "images/applyChanges.png")
+	root.iconphoto(False, logo)
 
-#main window frames
+except:
+	print("Critical error! Image files are missing. Please redownload the tool.")
+
+#window frames
 
 v = tk.StringVar()
 v.set(getLang)
@@ -340,56 +418,59 @@ unsavedChanges = tk.StringVar()
 mapFonts = "FixedSys"
 mapBG = "#89b0b3" ##89b0b3 38393F
 
-
 b = ttk.Style()
 b.configure("lang.TRadiobutton", padx=50, font="Helvetica, 9", background="#89b0b3")
 
-logoLabel = tk.Label(root, image=biglogo, height=300, bg="#89b0b3")
-logoLabel.pack(side="left")
+sidePanel = tk.Label(root, borderwidth=2, relief="groove", image=sidepanel, bg="#89b0b3")
+sidePanel.pack(side="left", fill="y", anchor="w")
 
-tabMain = ttk.Notebook(root, height=300, padding=3) 
-tabMain.pack(fill="both", expand=1)
+myNotebook = ttk.Notebook(root, takefocus=False) 
+myNotebook.pack(fill="both", expand=1)
 
+frame1 = tk.Frame(myNotebook, bg=mapBG, borderwidth=2, relief="groove")
 
-#frame1 = tk.Frame(tabMain, borderwidth=1, bg=mapBG)
-
-frame2 = tk.Frame(tabMain, bg=mapBG, height=300) 
+frame2 = tk.Frame(myNotebook, bg=mapBG, borderwidth=2, relief="groove") 
 frame2.pack(fill="both", expand=1)
 frame2.grid_columnconfigure(0, weight=1)
-frame2.grid_columnconfigure(5, weight=1)
+frame2.grid_columnconfigure(7, weight=1)
 
-#frame3 = tk.Frame(tabMain, borderwidth=1, bg=mapBG)
-#frame3.pack()
+frame3 = tk.Frame(myNotebook, bg=mapBG, borderwidth=2, relief="groove")
+frame3.pack(fill="both", expand=1)
+frame4 = tk.Frame(myNotebook, bg=mapBG, borderwidth=2, relief="groove")
 
-#tabMain.add(frame1, text="   Home   ")
-tabMain.add(frame2, text="   Map Selection   ")
-#tabMain.add(frame3, text="   Quick Bootup   ")
+tabPadding = 5
+myNotebook.add(frame1, text="   Home   ", image=tabHome, padding=tabPadding, state="disabled")
+myNotebook.add(frame2, text="   Map Selection   ", image=tabMap, padding=tabPadding)
+myNotebook.add(frame3, text="   Settings   ", image=tabSettings, padding=tabPadding)
+myNotebook.add(frame4, text="   Account Management   ", image=tabAcc, padding=tabPadding)
 
-############################################################################## Images on Screen
+######################################################################## Images on Frame2
 verIMG = tk.Label(frame2, bg=mapBG, image=verify)
-verIMG.place(x=10, y=3)
+verIMG.grid(row=0,column=1, columnspan=2, padx=(10,0))
 fazzChar = tk.Label(frame2, bg=mapBG, image=fazz)
-fazzChar.place(x=420)
+fazzChar.grid(row=0, column=4, padx=(0,125))
+fouad = tk.Label(frame2, bg=mapBG, image=fouadIMG)
+fouad.grid(row=1, column=5)
+courtyChar = tk.Label(frame2, bg=mapBG, image=courty)
+courtyChar.grid(row=1, column=1)
 selectIMG = tk.Label(frame2, image=selectMaps, bg=mapBG)
-selectIMG.place(x=589, y=95)
-dell = tk.Label(frame2, image=helle, bg=mapBG)
-dell.place(x=8, y=130)
+selectIMG.grid(row=3, rowspan=2, column=5, padx=(0,10), pady=(0,25))
+dripdell = tk.Label(frame2, image=engihelle, bg=mapBG)
+dripdell.grid(row=4, rowspan=2,column=1, padx=5)
 jigaudiChar = tk.Label(frame2, image=jigaudi, bg=mapBG)
-jigaudiChar.place(x=595, y=185)
-################################################################################################
+jigaudiChar.grid(row=5, column=5, columnspan=1, padx=(0,10))
+#######################################################################
 
+acbFolder = tk.Button(frame2, text="Set Game Directory Path", bg="#38393F", font=mapFonts, command=lambda: choosePath())
+acbFolder.grid(row=0, column=3, ipadx=20)
 
-acbFolder = tk.Button(frame2, text="Set Directory Path", bg="#38393F", font=mapFonts, command=lambda: choosePath())
-acbFolder.grid(row=0, column=1, columnspan=4, pady=(8,0))
+pathLabel = tk.Entry(frame2, textvariable=dirPath, bg="#38393F", font=mapFonts, borderwidth=3, justify="center", relief="sunken", state="readonly", takefocus=False)
+pathLabel.grid(row=1, column=2, columnspan=3,ipadx=200)
 
-pathLabel = tk.Entry(frame2, textvariable=dirPath, width=80, bg="#38393F", font=mapFonts, borderwidth=3, justify="center", relief="sunken", state="readonly")
-pathLabel.grid(row=1, column=1, columnspan=4, pady=(12,0))
+######################################### Frame for the maps and their switches
 
 mapFrame = tk.Frame(frame2 ,borderwidth=1, bg=mapBG, relief="sunken")
-mapFrame.grid(row=2, column=1, columnspan=4, pady=(13,0))
-
-
-
+mapFrame.grid(row=2, rowspan=3, column=2, columnspan=3)
 
 #SanDon
 labelSanDon = tk.Label(mapFrame, text="San Donato", font=mapFonts, bg=mapBG)
@@ -440,7 +521,7 @@ buttonPienza.grid(row=2, column=3, pady=(5,0))
 labelVenice = tk.Label(mapFrame, text="Venice", font=mapFonts, bg=mapBG)
 labelVenice.grid(row=3, column=2, padx=(25,0))
 buttonVenice = tk.Button(mapFrame, bg=mapBG, activebackground=mapBG, bd=0, image=onIMG, command=lambda: switchState("Venice"))
-buttonVenice.grid(row=3, column=3)
+buttonVenice.grid(row=3, column=3, pady=(5,0))
 
 
 #Forli
@@ -463,19 +544,19 @@ buttonRome = tk.Button(mapFrame, bg=mapBG, activebackground=mapBG, bd=0, image=o
 buttonRome.grid(row=2, column=5, pady=(5,0), padx=(0,30))
 
 
-
 changeLabel = tk.Label(frame2, textvariable=unsavedChanges, bg=mapBG, font="Arial, 8 italic", foreground="red")
-changeLabel.place(x=425, y=208)
+changeLabel.grid(row=5, column=2)
 
-applyChanges = tk.Button(frame2, text="Apply Changes", bg="#38393F", font=mapFonts, activeforeground="green", command=lambda: applyMapChanges())
-applyChanges.grid(row=3, column=2, columnspan=2, ipadx=5, pady=(15,0))
-startGame = tk.Button(frame2, text="Launch Multiplayer", bg="#38393F", font=mapFonts, command=lambda: launchGame(gamePath))
-startGame.grid(row=3, column=1, columnspan=2, ipadx=10, pady=(15,0))
+applyChanges = tk.Button(frame2, text="Apply Changes", bg=mapBG, activebackground=mapBG, bd=0, font=mapFonts, image=applyChangesIMG, command=lambda: applyMapChanges())
+applyChanges.grid(row=5, column=4)
 
-checkPathandStatus(gamePath)
-#print("\nGame path is: ", gamePath)
+startGame = tk.Button(frame2, text="Launch Multiplayer", bg=mapBG, activebackground=mapBG, bd=0, font=mapFonts, image=launchMP, command=lambda: launchGame(gamePath))
+startGame.grid(row=5, column=3)
 
-###############################################################################
+checkPathandStatus()
+print("Game path is: ", gamePath)
+
+############################################################################### Sets map status on tool start
 
 if mapStatus["Alhambra"] == True: buttonAlhambra.config(image=onIMG)
 else: buttonAlhambra.config(image=offIMG)
@@ -526,6 +607,7 @@ filemenu.add_command(label="Close", command=root.destroy)
 
 #settings menu
 settingsmenu.add_command(label="Change Interface Language", command=lambda: openLangMenu())
+
 
 #AN menu
 anmenu.add_command(label="Website", command=lambda: webbrowser.open(ANwebsite))
--- README.md
+++ README.md
@@ -3,4 +3,14 @@
 A tool to configure what maps to load with the game, and enable fast multiplayer bootup.
 More features might be added as dev work goes forward.
 
-Current version automatically detects your install path if it's the Steam version, otherwise you have to manually set it. Once it's set you can disable maps, apply the changes and press launch.
+Default Steam and Uplay/Ubisoft paths are automatically detected, otherwise manually set the directory path of your Assassin's Creed Brotherhood installation containining "ACBSP.exe".
+
+Once it's set you can disable maps, apply the changes and press launch.
+
+More features will be added soon.
+
+
+TO DO:
+
+Settings: Apply several other options
+Account Management: Choose from a range of accounts to play on. Automatically logs you into the said once you hit "Play Multiplayer".
